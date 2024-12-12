64352a652041a0b887a671b8724c95e07838363b
Gem <scanic29@ecolecatholique.ca>
2024-12-12 10:28:07 -0500

    Update booklet.js

--- js/booklet.js
+++ js/booklet.js
@@ -1,96 +1,93 @@
-const letters = [
-  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-];
-
-// Morse code mapping for each letter
-const morseCodeMap = {
-  "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".",
-  "F": "..-.", "G": "--.", "H": "....", "I": "..", "J": ".---",
-  "K": "-.-", "L": ".-..", "M": "--", "N": "-.", "O": "---",
-  "P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-",
-  "U": "..-", "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--",
-  "Z": "--.."
-};
-
-let currentLetter = ''; // Store the current letter
-
-function generateLetter() {
-  // Generate a random index to select a letter
-  const randomIndex = Math.floor(Math.random() * letters.length);
-  currentLetter = letters[randomIndex]; // Store the current letter
-  // Display the random letter
-  document.getElementById("letter").innerText = currentLetter;
-}
-
-// Call generateLetter when the page loads
-window.onload = function() {
-  generateLetter();
-};
-
-const resultMorseCode = document.getElementById('morseCode');
-var keyIsDown = false;
-var startTime;
-var endTime;
-var timer;
-var oscillator;
-
-function initializeAudio() {
-  var audioCtx = new AudioContext();
-  const gain = audioCtx.createGain();
-  gain.connect(audioCtx.destination);
-  gain.gain.value = 50;
-  oscillator = audioCtx.createOscillator();
-  oscillator.connect(gain);
-  oscillator.frequency.value = 520;
-}
-
-document.onkeydown = function (e) {
-  if (e.code == 'Space') {
-    if (!keyIsDown) { // if key is already down, ignore it
-      initializeAudio();
-      oscillator.start();
-      keyIsDown = true;
-      startTime = Date.now();
-      window.clearTimeout(timer);
-    }
-  }
-};
-
-document.onkeyup = function (e) {
-  if (e.code == 'Space') {
-    oscillator.stop();
-    keyIsDown = false;
-    endTime = Date.now();
-    if (endTime - startTime < 30) {
-      alert('Sorry! You are clicking too fast!');
-	  keyIsDown = false;
-      return;
-    }
-	if (endTime - startTime > 300) {
-		alert(`The dash was held to long, try again!`);
-		resultMorseCode.innerText = '';
+const morseLettersList = {
+    'a': ['.', '-'],
+    'b': ['-', '.', '.', '.'],
+    'c': ['-', '.', '-', '.',],
+    'd': ['-', '.', '.',],
+    'e': ['.'],
+    'f': ['.', '.', '-', '.'],
+    'g': ['-', '-', '.'],
+	'h': ['.', '.', '.', '.'],
+	'i': ['.', '.'],
+	'j': ['.', '-', '-', '-'],
+	'k': ['-', '.', '-'],
+	'l': ['.', '-', '.', '.'],
+	'm': ['-', '-'],
+	'n': ['-', '.'],
+	'o': ['-', '-', '-'],
+	'p': ['.', '-', '-', '.'],
+	'q': ['-', '-', '.', '-'],
+	'r': ['.', '-', '.'],
+	's': ['.', '.', '.'],
+	't': ['-'],
+	'u': ['.', '.', '-'],
+	'v': ['.', '.', '.', '-'],
+	'w': ['.', '-', '-'],
+	'x': ['-', '.', '.', '-'],
+	'y': ['-', '.', '-', '-'],
+	'z': ['-', '-', '.', '.'],
+	'1': ['.', '-', '-', '-', '-'],
+	'2': ['.', '.', '-', '-', '-'],
+	'3': ['.', '.', '.', '-', '-'],
+	'4': ['.', '.', '.', '.', '-'],
+	'5': ['.', '.', '.', '.', '.',],
+	'6': ['-', '.', '.', '.', '.'],
+	'7': ['-', '-', '.', '.', '.'],
+	'8': ['-', '-', '-', '.', '.',],
+	'9': ['-', '-', '-', '-', '.',],
+	'0': ['-', '-', '-', '-', '-',],
 	}
-    if ((endTime - startTime > 200) && (endTime - startTime < 300)) {
-      resultMorseCode.innerText += '-';
-    } else if ((endTime - startTime > 30) && (endTime - startTime < 180))  {
-      resultMorseCode.innerText += '.';
-    }
-	
-    timer = setTimeout(() => {
-      resultMorseCode.innerHTML += '&nbsp;';
-    }, 800);
-  } 
-  if (e.code == 'Enter') {
-    const userInput = resultMorseCode.innerText.trim().replace(/\s+/g, ' '); // Normalize spaces
-    const correctMorse = morseCodeMap[currentLetter];
-    if (userInput === correctMorse) {
-      alert('Correct! Well done!');
-    } else {
-      alert(`Incorrect! The correct Morse code for ${currentLetter} is ${correctMorse}`);
-    }
-    // Reset the Morse code input for the next attempt
-    resultMorseCode.innerText = '';
-    generateLetter(); // Generate a new letter
-  }
-};
+
+var isPlaying = false;
+
+function playMorseLetterSequence(sequence, index) {
+	console.log('index: ', index);
+    isPlaying = true;
+    var delay = 0.3 // dot
+    if (sequence[index] == '-') {
+        delay = 0.6; // dash
+    } else if (sequence[index] == ' ') {
+        delay = 1; // space
+	}
+	// https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
+    const audioCtx = new AudioContext();
+    const gain = audioCtx.createGain();
+    gain.connect(audioCtx.destination);
+    gain.gain.value = 50;
+    const oscillator = audioCtx.createOscillator();
+    oscillator.connect(gain);
+    oscillator.frequency.value = 520
+    oscillator.addEventListener('ended', () => {
+        if (sequence.length - 1 != index) {
+            playMorseLetterSequence(sequence, index + 1);
+        } else {
+            isPlaying = false;
+        }
+    });
+
+    oscillator.start(audioCtx.currentTime + 0.1);
+    oscillator.stop(audioCtx.currentTime + 0.1 + delay);
+}
+
+        function playLetter(letter) {
+            console.log('letter: ', letter);
+            console.log(morseLettersList[letter])
+            playMorseLetterSequence(morseLettersList[letter], 0)
+        }
+
+        // maybe keep track of all the buttons to disable them while the sound is playing
+        const letterButtons = []
+
+        var lettersContainer = document.getElementById('letters');
+        for (const letter in morseLettersList) {
+            // create all the buttons
+            var letterButton = document.createElement('button');
+            letterButton.innerHTML = letter;
+            letterButton.onclick = function () {
+                if (!isPlaying) {
+                    playLetter(letter);
+                    return false;
+                }
+            };
+            letterButtons.push(letterButton);
+            lettersContainer.appendChild(letterButton);
+        }
