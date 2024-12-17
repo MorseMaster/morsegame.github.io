10101c12c37ff89a1b52e83583922ffe394ac8c4
ApTPoP <nicmak17@ecolecatholique.ca>
2024-12-17 09:21:20 -0500

    Update translator.js

--- js/translator.js
+++ js/translator.js
@@ -1,4 +1,4 @@
-const morseCodeDict = {
+const morseLettersList = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
     'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
     'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
@@ -8,11 +8,11 @@ const morseCodeDict = {
     '9': '----.', '0': '-----', ' ': '/'
 };
 
-const reverseMorseCodeDict = Object.fromEntries(Object.entries(morseCodeDict).map(([key, value]) => [value, key]));
+const reverseMorseLettersList = Object.fromEntries(Object.entries(morseLettersList).map(([key, value]) => [value, key]));
 
 function textToMorse() {
     const textInput = document.getElementById('textInput').value.toUpperCase();
-    const morseOutput = textInput.split('').map(char => morseCodeDict[char] || '').join(' ');
+    const morseOutput = textInput.split('').map(char => morseLettersList[char] || '').join(' ');
     document.getElementById('morseOutput').innerText = morseOutput;
 }
 
@@ -20,7 +20,7 @@ function morseToText() {
     const morseInput = document.getElementById('morseInput').value.trim();
     const morseWords = morseInput.split(' / ');
     const decodedMessage = morseWords.map(morseWord => {
-        return morseWord.split(' ').map(code => reverseMorseCodeDict[code] || '').join('');
+        return morseWord.split(' ').map(code => reverseMorseLettersList[code] || '').join('');
     }).join(' ');
     document.getElementById('textOutput').innerText = decodedMessage;
 }
