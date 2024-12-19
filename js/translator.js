56b39d927df0da2b61c1766559d2d95f2790f780
ApTPoP <nicmak17@ecolecatholique.ca>
2024-12-19 12:49:05 -0500

    Update translator.js

--- js/translator.js
+++ js/translator.js
@@ -6,21 +6,21 @@ const morseLettersList = {
     'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
     '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
     '9': '----.', '0': '-----', ' ': '/'
-};
+}; // liste de l'alphabet morse
 
 const reverseMorseLettersList = Object.fromEntries(Object.entries(morseLettersList).map(([key, value]) => [value, key]));
 
-function textToMorse() {
-    const textInput = document.getElementById('textInput').value.toUpperCase();
-    const morseOutput = textInput.split('').map(char => morseLettersList[char] || '').join(' ');
-    document.getElementById('morseOutput').innerText = morseOutput;
+function textToMorse() { // fontion pout convertir de text en code morse
+    const textInput = document.getElementById('textInput').value.toUpperCase(); // prend l'entrée de 'utilisateur
+    const morseOutput = textInput.split('').map(char => morseLettersList[char] || '').join(' '); // isole les lettres, convertie les lettres en code morse, re-join les lettres
+    document.getElementById('morseOutput').innerText = morseOutput; // montre les resultats
 }
 
-function morseToText() {
-    const morseInput = document.getElementById('morseInput').value.trim();
-    const morseWords = morseInput.split(' / ');
-    const decodedMessage = morseWords.map(morseWord => {
-        return morseWord.split(' ').map(code => reverseMorseLettersList[code] || '').join('');
-    }).join(' ');
-    document.getElementById('textOutput').innerText = decodedMessage;
+function morseToText() { // fontion pout convertir de code morse en text
+    const morseInput = document.getElementById('morseInput').value.trim(); // prend l'entrée de 'utilisateur
+    const morseWords = morseInput.split(' / '); // isole les mots morse
+    const decodedMessage = morseWords.map(morseWord => { // trouve les bonnes lettres morse 
+    return morseWord.split(' ').map(code => reverseMorseLettersList[code] || '').join(''); // isole les lettres, convertie les lettres code morse en text, re-join les lettres
+    }).join(' '); // rejoin les mots
+    document.getElementById('textOutput').innerText = decodedMessage; // montre les resultats
 }
